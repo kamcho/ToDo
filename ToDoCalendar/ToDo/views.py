6a85547cd34645ce8df63ae3bcbcc99de5b370ad
kamcho <njokevin9@gmail.com>
2023-04-10 16:50:08 +0300

    proper routing

--- ToDoCalendar/ToDo/views.py
+++ ToDoCalendar/ToDo/views.py
@@ -1,9 +1,6 @@
-import random
 import secrets
-
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, DeleteView, UpdateView, ListView, DetailView
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Task
 
 # Create your views here.
@@ -17,16 +14,13 @@ class TaskList(ListView):
 
 class CreateTask(TemplateView):
     template_name = 'ToDo/create.html'
-
-    def post(self,request,*args,**kwargs):
+    def post(self,request):
         if request.method=="POST":
             title=request.POST.get('title')
             about=request.POST.get('about')
             date=request.POST.get('date')
             task=Task.objects.create(title=title,reminder=date,about=about)
-
             task.save()
-
             return redirect('tasks')
 
 class UpdateTask(DetailView):
@@ -37,52 +31,36 @@ class UpdateTask(DetailView):
             title=request.POST.get('title')
             about=request.POST.get('about')
             date=request.POST.get('date')
-            # task=Task.objects.create(title=title,reminder=date,about=about)
-            #
-            # task.save()
+
             if "edit" in request.POST:
                 task=Task.objects.get(id=self.kwargs['pk'])
                 task.title=title
                 task.about=about
                 task.reminder=date
                 task.save()
-                return redirect(f'tasks')
+                return redirect(request.path)
             else:
                 request.session['delete']=self.kwargs['pk']
                 return redirect(f'delete')
 
-
-            #
-            # task.save()
-
-            # return redirect('tasks')
-
-
-
-
-
-
 class DeleteTask(TemplateView):
     template_name = 'ToDo/delete.html'
-    code=""
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DeleteTask, self).get_context_data(**kwargs)
         self.code=secrets.token_hex(4)
         context['code']=self.code
         self.request.session['code']=self.code
         return context
-    def post(self,request,*args,**kwargs):
+    def post(self,request):
         if request.method=="POST":
             if "purge" in request.POST:
                 delete=request.POST.get('delete')
-                print(self.code)
                 if delete == request.session['code']:
-                    print("deleting\n\n\n")
                     task = Task.objects.get(id=request.session['delete']).delete()
-                    print(task)
                     return redirect('tasks')
                 else:
-                    return redirect('tasks')
+                    return redirect(request.path)
 
 
 
